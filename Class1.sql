4dd78e29be83f4c0748f1670ba421ea62d5f571f
Khushi Yadav <kky957242@gmail.com>
2025-12-03 14:14:39 +0530

    Create Class1.sql

---
+++ Class1.sql
@@ -0,0 +1,45 @@
+1. What is DBMS? (Database Management System)
+
+A DBMS is a software system used to store, manage, update, retrieve, and secure data in an organized manner.
+
+🔹 Why DBMS is Needed?
+
+Before DBMS, we used file systems, which had many problems:
+
+Data duplication
+
+No security
+
+No recovery
+
+Data inconsistency
+
+Slow access
+
+DBMS solves all these problems.
+
+🔹 Examples of DBMS:
+
+MySQL
+
+Oracle
+
+SQLite
+
+MongoDB
+
+SQL Server
+
+🔹 Advantages of DBMS:
+
+Data Security
+
+Data Sharing
+
+Backup & Recovery
+
+Reduced Redundancy
+
+Fast Access
+
+Data Integrity
