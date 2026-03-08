3f5948885d3a60d029d9402272d8149fd3533209
Khushi Yadav <kky957242@gmail.com>
2026-03-08 22:37:21 +0530

    Update Class2nd.sql

--- Class2nd.sql
+++ Class2nd.sql
@@ -2,7 +2,7 @@
 
 An index is a database structure that improves the speed of data retrieval operations on a table.
 Just like a book index helps you find a topic quickly, a database index helps locate rows faster.
-
+  
 -- Why do we use Indexes?
 
 Faster SELECT queries
