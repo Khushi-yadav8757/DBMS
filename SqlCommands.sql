ebec975dd1ff208fa1f02bbccbb4a9e307fe49a7
Khushi Yadav <kky957242@gmail.com>
2026-03-14 23:06:11 +0530

    Create SqlCommands.sql

---
+++ SqlCommands.sql
@@ -0,0 +1,13 @@
+--What are the types of SQL commands?
+
+Answer:
+
+DDL (Data Definition Language) – CREATE, ALTER, DROP
+
+DML (Data Manipulation Language) – INSERT, UPDATE, DELETE
+
+DQL (Data Query Language) – SELECT
+
+DCL (Data Control Language) – GRANT, REVOKE
+
+TCL (Transaction Control Language) – COMMIT, ROLLBACK
