81d67d75dc6079dba40dcae3cb2646f8b03d7157
Khushi Yadav <kky957242@gmail.com>
2026-03-20 20:08:58 +0530

    Create normalization.sql

---
+++ normalization.sql
@@ -0,0 +1,13 @@
+11. What is normalization?
+Process to remove redundancy and improve data integrity.
+
+12. Types of Normal Forms?
+
+1NF → No repeating groups
+
+2NF → No partial dependency
+
+3NF → No transitive dependency
+
+13. What is denormalization?
+Adding redundancy to improve performance.
