4dde0beb16f81692cd2deeb4a1394af63bd7e28b
Khushi Yadav <kky957242@gmail.com>
2026-03-20 20:10:00 +0530

    Create Acid_Property.sql

---
+++ Acid_Property.sql
@@ -0,0 +1,27 @@
+Transactions & Indexing
+
+18. What is a transaction?
+A set of operations treated as a single unit.
+
+19. What is ACID property?
+
+A → Atomicity
+
+C → Consistency
+
+I → Isolation
+
+D → Durability
+
+20. What is an index?
+Improves query speed (like a book index).
+
+🔹 Important Interview Questions (Asked Frequently)
+
+21. Difference between DELETE, TRUNCATE, DROP?
+
+DELETE → removes rows (can rollback)
+
+TRUNCATE → fast delete (no rollback)
+
+DROP → deletes table
