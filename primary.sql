81f88324ff523b47bc33c07888c4f98b96d873f8
Khushi Yadav <kky957242@gmail.com>
2026-03-20 20:07:00 +0530

    Create primary.sql

---
+++ primary.sql
@@ -0,0 +1,14 @@
+6. What is a Primary Key?
+Unique + NOT NULL → identifies each row.
+
+7. What is a Foreign Key?
+Links one table to another.
+
+8. What is Candidate Key?
+All possible keys that can be primary key.
+
+9. What is Super Key?
+Set of attributes that uniquely identify rows.
+
+10. What is Composite Key?
+Combination of multiple columns.
