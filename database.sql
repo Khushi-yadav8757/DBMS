559a92a1eaa652e92f3400907e4841d7d561ef09
Khushi Yadav <kky957242@gmail.com>
2026-03-20 20:06:15 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,17 @@
+1. What is DBMS?
+A Database Management System is software used to store, manage, and retrieve data efficiently (e.g., MySQL, Oracle).
+
+2. What is a database?
+A structured collection of data stored electronically.
+
+3. What is RDBMS?
+Relational DBMS stores data in tables (rows & columns) and uses relationships.
+
+4. Difference between DBMS and RDBMS?
+
+DBMS → No relation, simple storage
+
+RDBMS → Tables + relationships + constraints
+
+5. What is a table?
+A collection of rows and columns.
