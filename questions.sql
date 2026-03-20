f5af6a091430630f9a84b8ca3deb52da39b73b5b
Khushi Yadav <kky957242@gmail.com>
2026-03-20 20:09:28 +0530

    Create questions.sql

---
+++ questions.sql
@@ -0,0 +1,22 @@
+14. What is SQL?
+Language to interact with database.
+
+15. Types of SQL commands?
+
+DDL → CREATE, ALTER
+
+DML → INSERT, UPDATE
+
+DCL → GRANT
+
+TCL → COMMIT
+
+16. Difference between WHERE and HAVING?
+
+WHERE → before grouping
+
+HAVING → after grouping
+
+17. What are joins?
+Combine data from multiple tables
+Types: INNER, LEFT, RIGHT, FULL
