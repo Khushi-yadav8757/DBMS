68e36f610731851bbeb6c5d6066f901aa2a2f5cf
Khushi Yadav <kky957242@gmail.com>
2026-04-06 10:17:09 +0530

    Update normalization.sql

--- normalization.sql
+++ normalization.sql
@@ -2,12 +2,9 @@
 Process to remove redundancy and improve data integrity.
 
 12. Types of Normal Forms?
-
 1NF → No repeating groups
-
 2NF → No partial dependency
-
 3NF → No transitive dependency
-
+  
 13. What is denormalization?
 Adding redundancy to improve performance.
