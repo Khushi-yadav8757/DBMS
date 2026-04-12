1a4cfad292e50a2c55f667409725b13642476706
Khushi Yadav <kky957242@gmail.com>
2026-04-12 20:48:40 +0530

    Update SqlCommands.sql

--- SqlCommands.sql
+++ SqlCommands.sql
@@ -4,7 +4,6 @@ Answer:
 DDL (Data Definition Language) – CREATE, ALTER, DROP
 DML (Data Manipulation Language) – INSERT, UPDATE, DELETE
 DQL (Data Query Language) – SELECT
-  
 DCL (Data Control Language) – GRANT, REVOKE
   
 TCL (Transaction Control Language) – COMMIT, ROLLBACK
