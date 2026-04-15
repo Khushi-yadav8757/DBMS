e9c697ee7015c95e6b896019838f3290ab1e59a5
Khushi Yadav <kky957242@gmail.com>
2026-04-15 17:44:48 +0530

    Update Class3.sql

--- Class3.sql
+++ Class3.sql
@@ -1,4 +1,4 @@
-📘 DBMS Unit 3 – Transaction Management & Concurrency Control
+ DBMS Unit 3 – Transaction Management & Concurrency Control
 
 Unit-3 mainly deals with how databases handle multiple users safely and how data remains correct even if failures occur.
  
